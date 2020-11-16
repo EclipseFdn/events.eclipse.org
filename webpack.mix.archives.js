80fa50296261e5c8f27045ecb5b457f36f2300c4
Yi Liu <yi.liu@eclipse-foundation.org>
2020-11-16 14:28:06 -0500

    Add France 2013 #120
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>

--- webpack.mix.archives.js
+++ webpack.mix.archives.js
@@ -20,6 +20,7 @@ mix.less('./less/2006/eclipsecon/styles.less',  'static/archives/css/eclipsecon-
 mix.less('./less/2007/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2007.css');
 mix.less('./less/2008/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2008.css');
 mix.less('./less/2009/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2009.css');
+mix.less('./less/2013/france/styles.less', 'static/archives/css/eclipsecon-france-2013.css');
 mix.less('./less/2015/france/styles.less', 'static/archives/css/eclipsecon-france-2015.css');
 
 /*
@@ -28,7 +29,6 @@ mix.less('./less/2011/styles.less', './content/2011/styles.css');
 mix.less('./less/2012/styles.less', './content/2012/styles.css');
 mix.less('./less/2013/styles.less', './content/2013/styles.css');
 
-mix.less('./less/france2013/styles.less', './content/france2013/styles.css');
 mix.less('./less/france2014/styles.less', './content/france2014/styles.css');
 mix.less('./less/france2015/styles.less', './content/france2015/styles.css');
 mix.less('./less/france2016/styles.less', './content/france2016/styles.css');
