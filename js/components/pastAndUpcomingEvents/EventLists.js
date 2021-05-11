339c235e9f9a533b7fe24e44862004ca670e47e8
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-05-11 12:09:06 -0400

    Added label to indicate upcoming and past events

--- js/components/pastAndUpcomingEvents/EventLists.js
+++ js/components/pastAndUpcomingEvents/EventLists.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import EventCard from '../EventCard';
 import Loading from '../Loading';
 import PropTypes from 'prop-types';
@@ -13,18 +13,53 @@ const EventLists = ({ events, isFetchingMore, fetchMore, reachEnd }) => {
     );
   }
 
+  const [upcomingEvents, setUpcomingEvents] = useState([]);
+  const [pastEvents, setPastEvents] = useState([]);
+
+  const filterEvents = () => {
+    // get past event list
+    const thePastEvents = events.filter((eventItem) =>
+      checkDatePast(eventItem['end-date'])
+    );
+    setPastEvents(thePastEvents);
+
+    // get upcoming event list
+    const theUpcomingEvents = events.filter(
+      (eventItem) => !checkDatePast(eventItem['end-date'])
+    );
+    setUpcomingEvents(theUpcomingEvents);
+  };
+
+  const renderPastEvents = () => {
+    return pastEvents.map((eventItem) => (
+      <div className="col-md-12 max-min-width event-past" key={eventItem.id}>
+        <EventCard event={eventItem} />
+      </div>
+    ));
+  };
+
+  const renderUpcomingEvents = () => {
+    return upcomingEvents.map((eventItem) => (
+      <div className="col-md-12 max-min-width" key={eventItem.id}>
+        <EventCard event={eventItem} />
+      </div>
+    ));
+  };
+
+  useEffect(() => {
+    filterEvents();
+  }, [events]);
+
   return (
     <>
-      {events.map((event) => (
-        <div
-          className={`col-md-12 max-min-width ${
-            checkDatePast(event['end-date']) ? 'event-past' : ''
-          }`}
-          key={event.id}
-        >
-          <EventCard event={event} />
-        </div>
-      ))}
+      <div className="upcoming-event-list">
+        <h2>Upcoming Events</h2>
+        {renderUpcomingEvents()}
+      </div>
+      <div className="past-event-list">
+        <h2>Past Events</h2>
+        {renderPastEvents()}
+      </div>
 
       <div className="event-load-more">
         {reachEnd ? (
